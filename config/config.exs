79097a5cc52f148aada431e66886f9d58a7993e6
Sérgio Hilgert Junior <hilgertfacul@gmail.com>
2017-10-01 18:07:18 -0300

    end of the course

--- config/config.exs
+++ config/config.exs
@@ -28,8 +28,9 @@ import_config "#{Mix.env}.exs"
 
 config :ueberauth, Ueberauth,
   providers: [
-    github { Ueberauth.Strategy.Github, [] }
+    github: { Ueberauth.Strategy.Github, [] }
   ]
-config :ueberauth, Ueberauth.Strategy.Github.Oauth,
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
   client_id: System.get_env("GITHUB_CLIENT_ID"),
-  client_secret: System.get_env("GITHUB_SECRET_KEY")
+  client_secret: System.get_env("GITHUB_CLIENT_SECRET")
